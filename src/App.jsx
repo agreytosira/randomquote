ab14fa6b967d369cef27cda239307e45d08fb10f
Agrey Tosira <agreytosira@gmail.com>
2024-03-09 18:03:02 +0700

    feat: finish fetching and render data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('')
+  const getQuote = () => {
+    axios
+      .get('https://api.quotable.io/random')
+      .then((res) => setQuote(res.data))
+      .catch((err) => console.log(err))
+  }
+
+  const { author, content } = quote
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Random Quote</h1>
+      <button onClick={getQuote}>Get Quote</button>
+      <h2>{content}</h2>
+      <p>- {author}</p>
+    </div>
   )
 }
 
